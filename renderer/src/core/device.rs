24ac2301f8d5160c8dc59c47c5707a0ec3386503
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-10-21 20:26:04 +0200

    Squashed commit of the following:
    commit 9687ae7e32a7077578e04b852d4daab1cb52f620
    Author: Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
    Date:   Mon Oct 21 20:24:21 2024 +0200
    
        created and destroyed sync and command structs
    
    commit 5916f941cef58b8c90f9f6f008610305a7d62a53
    Author: Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
    Date:   Mon Oct 21 19:45:44 2024 +0200
    
        Created methods on device to create sync and command structures

--- renderer/src/core/device.rs
+++ renderer/src/core/device.rs
@@ -21,6 +21,64 @@ impl Device {
             graphics_idx,
         }
     }
+
+    pub fn create_command_pool(
+        &self,
+        flags: vk::CommandPoolCreateFlags,
+    ) -> Result<vk::CommandPool, vk::Result> {
+        let info = vk::CommandPoolCreateInfo::default()
+            .flags(flags)
+            .queue_family_index(self.graphics_idx);
+
+        unsafe { self.handle.create_command_pool(&info, None) }
+    }
+
+    pub fn allocate_command_buffer(
+        &self,
+        pool: vk::CommandPool,
+        level: vk::CommandBufferLevel,
+    ) -> Result<vk::CommandBuffer, vk::Result> {
+        let alloc_info = vk::CommandBufferAllocateInfo::default()
+            .level(level)
+            .command_pool(pool)
+            .command_buffer_count(1);
+
+        let buffers = unsafe { self.handle.allocate_command_buffers(&alloc_info) }?;
+        // TODO: support multiple buffers allocation in one go
+        Ok(buffers[0])
+    }
+
+    pub fn create_semaphore(
+        &self,
+        flags: vk::SemaphoreCreateFlags,
+    ) -> Result<vk::Semaphore, vk::Result> {
+        let info = vk::SemaphoreCreateInfo::default().flags(flags);
+        unsafe { self.handle.create_semaphore(&info, None) }
+    }
+
+    pub fn create_fence(&self, flags: vk::FenceCreateFlags) -> Result<vk::Fence, vk::Result> {
+        let info = vk::FenceCreateInfo::default().flags(flags);
+        unsafe { self.handle.create_fence(&info, None) }
+    }
+
+    pub fn wait_fence(&self, fence: vk::Fence, timeout: u64) -> Result<(), vk::Result> {
+        unsafe { self.handle.wait_for_fences(&[fence], true, timeout) }
+    }
+
+    pub fn reset_fence(&self, fence: vk::Fence) -> Result<(), vk::Result> {
+        unsafe { self.handle.reset_fences(&[fence]) }
+    }
+
+    pub fn destroy_command_pool(&self, pool: vk::CommandPool) {
+        unsafe { self.handle.destroy_command_pool(pool, None) };
+    }
+    pub fn destroy_semaphore(&self, semaphore: vk::Semaphore) {
+        unsafe { self.handle.destroy_semaphore(semaphore, None) };
+    }
+
+    pub fn destroy_fence(&self, fence: vk::Fence) {
+        unsafe { self.handle.destroy_fence(fence, None) };
+    }
 }
 
 impl Drop for Device {
--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ffi::CString;
 
-use ash::{ext, khr};
+use ash::{ext, khr, vk};
 use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 
 mod core;
@@ -65,13 +65,38 @@ mod core;
 
 // TODO: remove panics and unwrapping and move them to be handled in client code
 
+const MAX_FRAMES_IN_FLIGHT: usize = 2;
+
 // NOTE: rust calls Drop implementations in order of member declaration.
 // This is stupid imho but it is what it is
 #[allow(dead_code)]
 pub struct Renderer {
+    frame_number: usize,
+    frames: [FrameData; MAX_FRAMES_IN_FLIGHT],
     device: core::Device,
     surface: core::Surface,
     instance: core::Instance,
+}
+
+struct FrameData {
+    pub pool: vk::CommandPool,
+    pub buffer: vk::CommandBuffer,
+
+    pub swapchain_sem: vk::Semaphore,
+    pub render_sem: vk::Semaphore,
+    pub render_fen: vk::Fence,
+}
+
+impl Default for FrameData {
+    fn default() -> Self {
+        Self {
+            pool: vk::CommandPool::null(),
+            buffer: vk::CommandBuffer::null(),
+            swapchain_sem: vk::Semaphore::null(),
+            render_sem: vk::Semaphore::null(),
+            render_fen: vk::Fence::null(),
+        }
+    }
 }
 
 impl Renderer {
@@ -141,14 +166,60 @@ impl Renderer {
         };
         log::info!("Device created succesfully");
 
+        let frames = match Self::create_frames_structs(&device) {
+            Ok(val) => val,
+            Err(err) => {
+                log::error!("Failed to initialize frames data: {}", err);
+                panic!();
+            }
+        };
+
         Self {
+            frame_number: 0,
+            frames,
             instance,
             surface,
             device,
         }
     }
+
+    fn create_frames_structs(device: &core::Device) -> Result<[FrameData; 2], vk::Result> {
+        // Init frames data
+        let mut frames: [FrameData; MAX_FRAMES_IN_FLIGHT] =
+            [FrameData::default(), FrameData::default()];
+
+        for frame in &mut frames {
+            let pool =
+                device.create_command_pool(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER)?;
+            let buffer = device.allocate_command_buffer(pool, vk::CommandBufferLevel::PRIMARY)?;
+            let swapchain_sem = device.create_semaphore(vk::SemaphoreCreateFlags::default())?;
+            let render_sem = device.create_semaphore(vk::SemaphoreCreateFlags::default())?;
+            let render_fen = device.create_fence(vk::FenceCreateFlags::SIGNALED)?;
+
+            frame.pool = pool;
+            frame.buffer = buffer;
+            frame.render_fen = render_fen;
+            frame.render_sem = render_sem;
+            frame.swapchain_sem = swapchain_sem;
+        }
+
+        Ok(frames)
+    }
+
+    fn get_current_frame(&self) -> &FrameData {
+        &self.frames[self.frame_number % MAX_FRAMES_IN_FLIGHT]
+    }
 }
 
 impl Drop for Renderer {
-    fn drop(&mut self) {}
+    fn drop(&mut self) {
+        log::trace!("Destroying Renderer");
+
+        for frame in &self.frames {
+            self.device.destroy_command_pool(frame.pool);
+            self.device.destroy_semaphore(frame.swapchain_sem);
+            self.device.destroy_semaphore(frame.render_sem);
+            self.device.destroy_fence(frame.render_fen);
+        }
+    }
 }
